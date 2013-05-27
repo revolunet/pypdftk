5edf012f211b81a443ba004243b1c4f2908efe08
LaurentMox <laurent@revolunet.com>
2013-05-27 11:13:51 +0200

    Version correction

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name='pypdftk',
     description='''Python wrapper for PDFTK''',
     long_description=open('README.md').read(),
-    version='0.1dev',
+    version='0.2',
     author='Julien Bouquillon',
     author_email='julien@revolunet.com',
     url='http://github.com/revolunet/pypdftk',
