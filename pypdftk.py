c0bd6ab2f016813d1ba65d80d52fc6445d4a7902
Julien Bouquillon <julien@bouquillon.com>
2013-06-04 14:48:34 +0200

    fix py2.6 compat

--- pypdftk.py
+++ pypdftk.py
@@ -14,10 +14,23 @@ import shutil
 
 PDFTK_PATH = '/usr/bin/pdftk'
 
+def check_output(*popenargs, **kwargs):
+    if 'stdout' in kwargs:
+        raise ValueError('stdout argument not allowed, it will be overridden.')
+    process = subprocess.Popen(stdout=subprocess.PIPE, *popenargs, **kwargs)
+    output, unused_err = process.communicate()
+    retcode = process.poll()
+    if retcode:
+        cmd = kwargs.get("args")
+        if cmd is None:
+            cmd = popenargs[0]
+        raise subprocess.CalledProcessError(retcode, cmd, output=output)
+    return output
+
 
 def run_command(command, shell=False):
     ''' run a system command and yield output '''
-    p = subprocess.check_output(command, shell=shell)
+    p = check_output(command, shell=shell)
     return p.split('\n')
 
 
@@ -112,3 +125,4 @@ def gen_xfdf(datas={}):
     f.write(tpl.encode('UTF-8'))
     f.close()
     return out_file
+
