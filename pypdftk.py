dbc39dd4acdef2354fa97ae1508d07a154dfb00d
Adam Lehigh <adam@dominantseventh.net>
2020-06-02 12:48:40 -0400

    Return a list from dump_data_fields() when the PDF form has multiple values.
    This resolves Github issue #32.
    The test files form.json and form-filled.json
    were also updated to match the new output.

--- pypdftk.py
+++ pypdftk.py
@@ -84,14 +84,32 @@ def fill_form(pdf_path, datas={}, out_file=None, flatten=True):
     os.remove(tmp_fdf)
     return out_file
 
-def dump_data_fields(pdf_path):
+def dump_data_fields(pdf_path, add_id=False):
     '''
         Return list of dicts of all fields in a PDF.
+        If multiple values with the same key are provided for some fields (like
+        FieldStateOption), the data for that key will be a list.
+        If id is True, a unique numeric ID will be added for each PDF field.
     '''
     cmd = "%s %s dump_data_fields" % (PDFTK_PATH, pdf_path)
     field_data = map(lambda x: x.split(': ', 1), run_command(cmd, True))
     fields = [list(group) for k, group in itertools.groupby(field_data, lambda x: len(x) == 1) if not k]
-    return [dict(f) for f in fields]
+    field_data = []  # Container for the whole dataset
+    for i, field in enumerate(fields):  # Iterate over datasets for each PDF field.
+        d = {}  # Use a dictionary as a container for the data from one PDF field.
+        if add_id:
+            d = {'id': i}
+        for i in sorted(field):  # Sort the attributes of the PDF field, then loop through them.
+            # Each item i has 2 elements: i[0] is the key (attribute name), i[1] is the data (value).
+            if i[0] in d:  # If the key is already present in the dictionary...
+                if isinstance(d[i[0]], list):  # ...and the value is already a list...
+                    d[i[0]].append(i[1])  # ...just append to it.
+                else:  # Otherwise (if the value isn't already a list)...
+                    d[i[0]] = [ d[i[0]], i[1] ]  # ...create a new list with the original and new values.
+            else:  # Otherwise (the key isn't already present in the dictionary)...
+                d[i[0]] = i[1]  # ...simply add it to the dictionary.
+        field_data.append(d)  # Finally, add the dictionary for this field to the big container.
+    return field_data
 
 def concat(files, out_file=None):
     '''
